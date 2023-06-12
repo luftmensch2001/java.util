963a36a2a53cac14117dfecaa429c2b18ea61bd5
phuongtd@runsystem.net <phuongtd@runsystem.net>
2023-06-12 10:19:47 +0700

    List

---
+++ src/List.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class List {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        System.out.println(myList);
+    }
+}
